b1bd82c6e3396ee034a0ba3c103c267284d55747
Jun Wen <khoongjunwen@gmail.com>
2020-11-18 14:51:40 +0800

    git:commit

--- display_weeklyTimeslot.php
+++ display_weeklyTimeslot.php
@@ -2,20 +2,40 @@
     include 'includes/db.connect.php'
 ?>
 
-<?php
-    
+<!DOCTYPE html>
+<html>
+    <head></head>
 
-    $sql = "SELECT id, student_id, student_name, teacher_name , sem_name , from_time , to_time FROM timeslot";
-    $result = $conn->query($sql);
+    <body>
+        <h1>display Weekly Timeslot</h1>
+        <?php
+            $query = "SELECT * FROM timeslot";
+            $data = mysqli_query($connect,$query);
+            $total = mysqli_num_rows($data);
 
-    if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo   $row["id"]. ") " . " - Student ID) " . $row["student_id"]. " - Student Name)" . $row["student_name"]. " - Teacher Name) " 
-        . $row["teacher_name"]. " - Sem) " . $row["sem_name"] . " - from) " . $row["from_time"] . " - to) " . $row["to_time"]  . "<br><br>";
-    }
-    } else {
-    echo "0 results";
-    }
-    $conn->close();
-?>
+            if($total!=0){
+                while($result=mysqli_fetch_assoc($data)){
+                    echo "
+                    <tr>
+                    <tr>ID:&nbsp".$result['id']."&nbsp</tr>
+                    <tr>Student ID:&nbsp".$result['student_id']."&nbsp</tr>
+                    <tr>Student Name:&nbsp".$result['student_name']."&nbsp</tr>
+                    <tr>Teacher:&nbsp".$result['teacher_name']."&nbsp</tr>
+                    <tr>Sem:&nbsp".$result['sem_name']."&nbsp</tr>
+                    <tr>From:&nbsp	".$result['from_time']."</tr>
+                    <tr>To:&nbsp	".$result['to_time']."</tr>
+                    
+                    <tr><a href='?id=$result[id]'>Decline</a></tr>
+                
+                   <br>
+                   <br>
+                </tr>
+                ";
+                }
+            }
+
+
+        ?>
+    </body>
+
+</html>
