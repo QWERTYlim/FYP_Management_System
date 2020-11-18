4aa0a6b6ee4ddd2aa02e6e2f325172b4055dc6b6
qwertylimau <d180293b@sc.edu.my>
2020-11-18 15:37:29 +0800

    n

--- index.php
+++ index.php
@@ -12,13 +12,9 @@
 <button class='blue'><a href='appointment_approve.php' class="blue">Approve</a></button>
 <br>
 <br>
-<<<<<<< HEAD
-<button class='blue'><a href='weekly_timeslot.php'>Weekly Timeslot</a></button>
-<br>
-<br>
-<button class='blue'><a href='display_weeklyTimeslot.php'>Display Weekly Timeslot</a></button>
-=======
 <button class='blue'><a href='weekly_timeslot.php' class="blue">Weekly Timeslot</a></button>
->>>>>>> e9b99fc21aa9c21e1f50057d7eca554f7f70e97a
+<br>
+<br>
+<button class='blue'><a href='display_weeklyTimeslot.php' class="blue"> Display Weekly Timeslot</a></button>
 </body>
 </html>
