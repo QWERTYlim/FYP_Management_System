aadbd6642f76c51374d9ee79715c499679cfb610
qwertylimau <d180293b@sc.edu.my>
2020-11-18 15:01:21 +0800

    change

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
 </head>
 <body>
 
-<button class='blue'><a href='appointment.php'>Make Appointment</a></button>
+<button class='blue'><a href='appointment.php' class="blue">Make Appointment</a></button>
 <br>
 <br>
-<button class='blue'><a href='appointment_approve.php'>Approve</a></button>
+<button class='blue'><a href='appointment_approve.php' class="blue">Approve</a></button>
 <br>
 <br>
-<button class='blue'><a href='weekly_timeslot.php'>Weekly Timeslot</a></button>
+<button class='blue'><a href='weekly_timeslot.php' class="blue">Weekly Timeslot</a></button>
 </body>
 </html>
