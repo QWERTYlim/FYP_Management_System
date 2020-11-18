e442eb93cba83d39eb5de4d55e9d28ba8af8e82c
Jun Wen <khoongjunwen@gmail.com>
2020-11-18 14:59:11 +0800

    git:commit

--- display_weeklyTimeslot.php
+++ display_weeklyTimeslot.php
@@ -4,38 +4,61 @@
 
 <!DOCTYPE html>
 <html>
-    <head></head>
+    <head>
+    <title>Appointment Approve</title>
+    <link rel="stylesheet" type="text/css" href="css/blue.button.css">
+    <link rel="stylesheet" type="text/css" href="css/table.css">
+    <button class="blue"><a href="index.php">Home</a></button>
+
+    </head>
 
     <body>
         <h1>display Weekly Timeslot</h1>
-        <?php
+        <table>
+            <tr>
+            <th>ID</th>
+            <th>Student ID</th>
+            <th>Student Name</th>
+            <th>Teacher</th>
+            <th>Sem</th>
+            <th>From</th>
+            <th>To</th>
+
+            <th>Delete</th>
+            </tr>
+
+            <?php
             $query = "SELECT * FROM timeslot";
             $data = mysqli_query($connect,$query);
             $total = mysqli_num_rows($data);
+
+            
 
             if($total!=0){
                 while($result=mysqli_fetch_assoc($data)){
                     echo "
                     <tr>
-                    <tr>ID:&nbsp".$result['id']."&nbsp</tr>
-                    <tr>Student ID:&nbsp".$result['student_id']."&nbsp</tr>
-                    <tr>Student Name:&nbsp".$result['student_name']."&nbsp</tr>
-                    <tr>Teacher:&nbsp".$result['teacher_name']."&nbsp</tr>
-                    <tr>Sem:&nbsp".$result['sem_name']."&nbsp</tr>
-                    <tr>From:&nbsp	".$result['from_time']."</tr>
-                    <tr>To:&nbsp	".$result['to_time']."</tr>
-                    
-                    <tr><a href='?id=$result[id]'>Decline</a></tr>
-                
+                    <th>&nbsp".$result['id']."</th>
+                    <th>&nbsp".$result['student_id']."</th>
+                    <th>&nbsp".$result['student_name']."</th>
+                    <th>&nbsp".$result['teacher_name']."</th>
+                    <th>&nbsp".$result['sem_name']."</th>
+                    <th>&nbsp".$result['from_time']."</th>
+                    <th>&nbsp".$result['to_time']."</th>
+                    <th><a href='delete.php?id=$result[id]'>Delete</a></th>
                    <br>
                    <br>
                 </tr>
                 ";
                 }
             }
-
-
-        ?>
+            else{
+                echo "0 result";
+            }
+            ?>
+        </table>
+        
+        
     </body>
 
 </html>
