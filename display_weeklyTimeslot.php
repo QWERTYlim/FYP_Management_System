ea331154193b01373c444cced0bd7f42cb7c59fa
Jun Wen <khoongjunwen@gmail.com>
2020-11-19 10:06:17 +0800

    ???

--- display_weeklyTimeslot.php
+++ display_weeklyTimeslot.php
@@ -14,20 +14,54 @@
 
     <body>
         <h1>display Weekly Timeslot</h1>
-        <table>
-            <tr>
-            <th>ID</th>
-            <th>Student ID</th>
-            <th>Student Name</th>
-            <th>Teacher</th>
-            <th>Sem</th>
-            <th>From</th>
-            <th>To</th>
-
-            <th>Delete</th>
-            </tr>
 
             <?php
+            $status=$_GET["status"];
+            if($status=="disp")
+            {
+            $connect = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
+            $query = "SELECT * FROM timeslot";
+            $res = mysqli_query($connect,$query); 
+            
+            echo "<table>";
+            echo "<tr>";
+                echo "<td>";echo"ID";echo "</td>";
+                echo "<td>";echo"Student ID";echo "</td>";
+                echo "<td>";echo"Student Name";echo "</td>";
+                echo "<td>";echo"Teacher";echo "</td>";
+                echo "<td>";echo"Sem";echo "</td>";
+                echo "<td>";echo"From";echo "</td>";
+                echo "<td>";echo"To";echo "</td>";
+                echo "<td>";echo"Delete";echo "</td>";
+            echo"</tr>";
+            while($row=mysqli_fetch_array($res)){
+                echo "<tr>";
+                echo "<td>";echo $row["id"];echo "</td>";
+                echo "<td>";echo $row["student_id"];echo "</td>";
+                echo "<td>";echo $row["student_name"];echo "</td>";
+                echo "<td>";echo $row["teacher_name"];echo "</td>";
+                echo "<td>";echo $row["sem_name"];echo "</td>";
+                echo "<td>";echo $row["from_time"];echo "</td>";
+                echo "<td>";echo $row["to_time"];echo "</td>";
+                echo "<td>";?>
+                <input type="button" id="<?php echo $row["id"]; ?>" name="<?php echo $row["id"];?>" value="Decline" onclick="decline(this.id)">
+                <input type="button" id="<?php echo $row["id"]; ?>" name="<?php echo $row["id"];?>" value="Accept" onclick="accept(this.id)">
+            
+                <?php echo "</td";
+                echo"</tr>";
+             }
+             echo "</table>";
+            
+            }
+            if($status=="delete"){
+                $connect = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
+                $id=$_GET["id"];
+                $query = "DELETE FROM appointment where id=$id";
+                $res = mysqli_query($connect,$query); 
+            }
+
+
+/*
             $query = "SELECT * FROM timeslot";
             $data = mysqli_query($connect,$query);
             $total = mysqli_num_rows($data);
@@ -55,8 +89,9 @@
             else{
                 echo "0 result";
             }
+*/
             ?>
-        </table>
+        
         
         
     </body>
