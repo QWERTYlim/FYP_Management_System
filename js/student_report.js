c7beb46955b3d617e723e39bcf7472bed1969e05
qwertylimau <d180293b@sc.edu.my>
2021-01-13 13:44:15 +0800

    delete all

--- js/student_report.js
+++
@@ -1,9 +0,0 @@
-disp_data()
- 	function disp_data()
- 	{
- 		var xmlhttp=new XMLHttpRequest();
- 		xmlhttp.open("GET","../function/ajax_student_report.php?status=disp",false);
- 		xmlhttp.send(null);
- 		document.getElementById("disp_data").innerHTML=xmlhttp.responseText; 
-	 }
-
