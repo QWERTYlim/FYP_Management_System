16e6ae06d1beea9f28dae5fa0ece2d06f0e0483c
JunWenK <khoongjunwen@gmail.com>
2021-01-15 00:15:21 +0800

    past year report leftjoin

--- function/stu_ref_fetch.php
+++ function/stu_ref_fetch.php
@@ -4,14 +4,16 @@ $output = '';
 if(isset($_POST["query"]))
 {
 	$search = mysqli_real_escape_string($connect, $_POST["query"]);
-	$query = "
-	SELECT * FROM uploadref
-	WHERE title LIKE '%".$search."%'";
+	
+	$query = "SELECT * FROM uploadreport WHERE filetitle LIKE '%".$search."%'  AND Rating>80
+	OR name LIKE '%".$search."%' AND Rating>80 ";
+	
+
 }
 else
 {
 	$query = "
-	SELECT * FROM uploadref ORDER BY name";
+	SELECT * FROM uploadreport LEFT JOIN studentinfo ON uploadreport.sid=studentinfo.StudentID WHERE Rating>80";
 }
 $result = mysqli_query($connect, $query);
 if(mysqli_num_rows($result) > 0)
@@ -19,18 +21,23 @@ if(mysqli_num_rows($result) > 0)
 	$output .= '<div class="table-responsive">
 					<table class="table table bordered">
 						<tr>
+						<th>Student ID</th>
 						<th>Title</th>
 							<th>File Name</th>
+							<th>date</th>
 							<th>Download</th>
-							
+							<th>PhoneNumber</th>
 						</tr>';
 	while($row = mysqli_fetch_array($result))
 	{
 		$output .= '
 			<tr>
-			<td>'.$row["title"].'</td>
-                <td>'.$row["name"].'</td>
-                 <td><a href="../function/stu_ref_fetch.php?file_name='.$row["name"].'">Download</a></td>
+			<td>'.$row["sid"].'</td>
+			<td>'.$row["filetitle"].'</td>
+				<td>'.$row["name"].'</td>
+				<td>'.$row["date"].'</td>
+				 <td><a href="../function/stu_ref_fetch.php?file_name='.$row["name"].'">Download</a></td>
+				 <td>'.$row["PhoneNumber"].'</td>
 				 
 			</tr>	
 		';
@@ -47,10 +54,10 @@ if (isset($_GET['file_name'])) {
     $name = $_GET['file_name'];
     
     // fetch file to download from database
-    $sql = "SELECT * FROM uploadref WHERE name=$name";
+    $sql = "SELECT * FROM uploadreport WHERE name=$name";
     $result = mysqli_query($connect, $sql);
     
-    $filepath = '../ref_upload/' . $name;
+    $filepath = '../report_upload/' . $name;
     echo'<script> alert("Wrong!")</script>';
 
 
@@ -61,8 +68,8 @@ if (isset($_GET['file_name'])) {
         header('Expires: 0');
         header('Cache-Control: must-revalidate');
         header('Pragma: public');
-        header('Content-Length: ' . filesize('../ref_upload/' . $name));
-        readfile('../ref_upload/' . $name);
+        header('Content-Length: ' . filesize('../report_upload/' . $name));
+        readfile('../report_upload/' . $name);
         exit;
     }
 
